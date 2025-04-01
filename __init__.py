5c65dd1c8f20a38205f5ff6ed221039d36ae2ac1
Nik Clark <24965799+zzubnik@users.noreply.github.com>
2025-04-01 18:57:11 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,15 +1,21 @@
 from .TT_FemaleClothes import TT_FemaleClothes
 from .TT_RandomPlacesOnEarth import TT_RandomPlacesOnEarth
 from .TT_CountryOfOrigin import TT_CountryOfOrigin
+from .TT_StoryCombiner import TT_StoryCombiner
+from .TT_TextInput import TT_TextInput
 
 NODE_CLASS_MAPPINGS = {
     "TT_FemaleClothes": TT_FemaleClothes,
     "TT_RandomPlacesOnEarth": TT_RandomPlacesOnEarth,
     "TT_CountryOfOrigin": TT_CountryOfOrigin,
+    "TT_StoryCombiner": TT_StoryCombiner,
+    "TT_TextInput": TT_TextInput,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "TT_FemaleClothes": "TT_FemaleClothes",
     "TT_RandomPlacesOnEarth": "TT_RandomPlacesOnEarth",
     "TT_CountryOfOrigin": "TT_CountryOfOrigin",
+    "TT_StoryCombiner": "TT_StoryCombiner",
+    "TT_TextInput": "TT_Text Input",
 }
